a9f02625bd56a097b51159d47adefff0932f6e15
LucasGabrielSousaCosta <163887555+LucasGabrielSousaCosta@users.noreply.github.com>
2024-03-22 17:51:47 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,16 +4,29 @@ public class Main
     public static void main(String[] args)
     {
         Fila fila = new Fila();
-        fila.add(1);
-        fila.add(2);
-        fila.add(3);
-        fila.add(4);
-        fila.clear();
+        fila.add(10);
+        fila.add(20);
+        fila.add(30);
+        fila.add(40);
+        fila.add(50);
+        fila.add(60);
+        System.out.println("IMPRIMIR FILA:");
+        fila.print();
+        System.out.println("\n\nFILA CONTEM O VALOR 20:");
+        System.out.println(fila.contains(20));
+        fila.remove();
+        fila.remove();
+        System.out.println("\nPRINT DEPOIS DE REMOVER OS 2 PRIMEIROS:");
+        fila.print();
+        System.out.println("\n\nFILA PEEK,SIZE,ESTÁ VAZIA E CONTEM O VALOR 20");
+        System.out.println(fila.peek());
         System.out.println(fila.size());
-        if(fila.isEmpty())
-            System.out.println("Lista vazia.");
-        else
-            System.out.println("Lista não está vazia.");
-        
+        System.out.println(fila.isEmpty());
+        System.out.println(fila.contains(20));
+        fila.clear();
+        System.out.println("\nCLEAR + PRINT + PEEK + SIZE");
+        fila.print();
+        System.out.println(fila.peek());
+        System.out.println(fila.size());
     }
 }
